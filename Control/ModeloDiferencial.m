89b2a868419c738f862094f7dbc0814ec7d5b065
Charlie5DH <crmorales5dh@gmail.com>
2020-07-26 17:13:12 -0300

    removed unnecessary files

--- Control/ModeloDiferencial.m
+++
@@ -1,7 +0,0 @@
-function [dx,dy,dcita] = ModeloDiferencial(cita,u1,u2)
-dcita = u1;
-dx = cos(cita)*u2;
-dy = sin(cita)*u2;
-
-end
-
--- Control/modeloAckermanFWD.m
+++
@@ -1,7 +0,0 @@
-function [dx,dy,dcita,dfi] = modeloAckermanFWD(x,y,cita,fi,u1,u2,L)
-dx = cos(fi)*cos(cita)*u2;
-dy = cos(fi)*sin(cita)*u2;
-dcita = sin(fi)*u2/L;
-dfi = u1;
-end
-
